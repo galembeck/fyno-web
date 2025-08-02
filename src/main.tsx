c26f6c6328be32dd678d5d27186cb90b542b984b
Pedro Galembeck <pedro.galembeck@mblabs.com.br>
2025-08-02 13:22:06 -0300

    feat: create navbar component

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { routeTree } from "./route-tree-gen.ts";
 
 import "./index.css";
 
-import { App } from "./app";
+const router = createRouter({ routeTree });
 
-// biome-ignore lint/style/noNonNullAssertion: required by react
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+// biome-ignore lint/style/noNonNullAssertion: required by @Vite
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>
+	);
+}
