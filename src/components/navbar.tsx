b6da799900c271f344948c0e7f47283c872751ef
Pedro Galembeck <pedro.galembeck@mblabs.com.br>
2025-08-02 14:21:44 -0300

    fix: update navbar sticky position to display outlet content

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -15,7 +15,7 @@ export function Navbar() {
 	};
 
 	return (
-		<nav className="fixed top-0 right-0 left-0 z-50 bg-primary-black shadow-lg">
+		<nav className="sticky top-0 right-0 left-0 z-50 bg-primary-black shadow-lg">
 			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 				<div className="flex items-center justify-between py-6">
 					<Link className="flex gap-2" to="/">
