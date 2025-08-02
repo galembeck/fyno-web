1fb83a15127d047b38c36ea432c1ccda08a04bf8
Pedro Galembeck <pedro.galembeck@mblabs.com.br>
2025-08-02 14:10:24 -0300

    fix: update App & main structures

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,15 @@
-// export function App() {
-// 	return (
-// 		<div>
-// 			<h1>Fyno</h1>
-// 		</div>
-// 	);
-// }
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+
+import { routeTree } from "./route-tree-gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+export function App() {
+	return <RouterProvider router={router} />;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,13 @@
-import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
-import { routeTree } from "./route-tree-gen.ts";
+import { createRoot } from "react-dom/client";
+
+import { App } from "./app.tsx";
 
 import "./index.css";
 
-const router = createRouter({ routeTree });
-
-declare module "@tanstack/react-router" {
-	interface Register {
-		router: typeof router;
-	}
-}
-
-// biome-ignore lint/style/noNonNullAssertion: required by @Vite
-const rootElement = document.getElementById("root")!;
-if (!rootElement.innerHTML) {
-	const root = ReactDOM.createRoot(rootElement);
-	root.render(
-		<StrictMode>
-			<RouterProvider router={router} />
-		</StrictMode>
-	);
-}
+// biome-ignore lint/style/noNonNullAssertion: required by React
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
