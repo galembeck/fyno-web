6912ee139cb0661ad712097bb437af3aec783cd7
Pedro Galembeck <pedro.galembeck@mblabs.com.br>
2025-08-03 14:01:01 -0300

    feat: create landing page content & layout

---
+++ src/pages/_landing/layout.tsx
@@ -0,0 +1,23 @@
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { Footer } from "@/components/footer";
+import { Navbar } from "@/components/navbar";
+
+export const Route = createFileRoute("/_landing")({
+	component: Layout,
+});
+
+function Layout() {
+	return (
+		<main className="min-h-screen w-full bg-primary-dark text-white">
+			<div>
+				<Navbar />
+
+				<div>
+					<Outlet />
+				</div>
+
+				<Footer />
+			</div>
+		</main>
+	)
+}
