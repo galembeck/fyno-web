64378e731364d5f72f8b5ba5263fa2f7c1225498
Pedro Galembeck <pedro.galembeck@mblabs.com.br>
2025-08-03 14:03:23 -0300

    feat: create dashboard page content & layout

--- src/pages/_app/layout.tsx
+++ src/pages/_app/layout.tsx
@@ -1,5 +1,21 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
-import { Navbar } from "@/components/navbar";
+import { AppSidebar } from "@/components/sidebar/app-sidebar";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeToggle } from "@/components/theme-toggle";
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 export const Route = createFileRoute("/_app")({
 	component: Layout,
@@ -7,14 +23,40 @@ export const Route = createFileRoute("/_app")({
 
 function Layout() {
 	return (
-		<main className="min-h-screen w-full bg-primary-dark text-white">
-			<div>
-				<Navbar />
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<div className="min-h-screen w-full dark:bg-app-background dark:text-white">
+				<SidebarProvider>
+					<AppSidebar />
+					<SidebarInset>
+						<header className="flex h-16 shrink-0 items-center justify-between gap-2 pr-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+							<div className="flex items-center gap-2 px-4">
+								<SidebarTrigger className="-ml-1" />
+								<Separator
+									className="mr-2 data-[orientation=vertical]:h-4"
+									orientation="vertical"
+								/>
+								<Breadcrumb>
+									<BreadcrumbList>
+										<BreadcrumbItem className="hidden md:block">
+											<BreadcrumbLink href="#">
+												Building Your Application
+											</BreadcrumbLink>
+										</BreadcrumbItem>
+										<BreadcrumbSeparator className="hidden md:block" />
+										<BreadcrumbItem>
+											<BreadcrumbPage>Data Fetching</BreadcrumbPage>
+										</BreadcrumbItem>
+									</BreadcrumbList>
+								</Breadcrumb>
+							</div>
 
-				<div>
-					<Outlet />
-				</div>
+							<ThemeToggle />
+						</header>
+
+						<Outlet />
+					</SidebarInset>
+				</SidebarProvider>
 			</div>
-		</main>
+		</ThemeProvider>
 	);
 }
